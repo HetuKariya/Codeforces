9e5ddfa42535f4d5fc4db83066c4163e211560e2
HetuKariya <131229641+HetuKariya@users.noreply.github.com>
2025-05-20 15:41:41 +0530

    Added a Question

---
+++ Farmer_John's_Challenge_1942A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t, n, k;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> k;
+        if (n == k)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                cout << k << " ";
+            }
+        }
+        else if (k < n && k == 1)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                cout << i << " ";
+            }
+        }
+        else
+            cout << -1;
+        cout << "\n";
+    }
+    return 0;
+}
--- Stair_Peak_or_Neither_1950A.cpp
+++ Stair_Peak_or_Neither_1950A.cpp
@@ -1,24 +1,21 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     long long t, a, b, c;
     cin >> t;
     while(t--)
     {
         cin >> a >> b >> c;
         if (a < b && b < c)
-        {
             cout << "STAIR" << endl;
-        }
         else if (a < b && b > c)
-        {
             cout << "PEAK" << endl;
-        }
         else
-        {
             cout << "NONE" << endl;
-        }
     }
     return 0;
 }
