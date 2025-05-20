8f54e301a3fdcdf8d23b7a89448e4e0a3a9debb2
HetuKariya <131229641+HetuKariya@users.noreply.github.com>
2025-05-20 15:22:48 +0530

    Initial Commit

---
+++ Stair_Peak_or_Neither_1950A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long t, a, b, c;
+    cin >> t;
+    while(t--)
+    {
+        cin >> a >> b >> c;
+        if (a < b && b < c)
+        {
+            cout << "STAIR" << endl;
+        }
+        else if (a < b && b > c)
+        {
+            cout << "PEAK" << endl;
+        }
+        else
+        {
+            cout << "NONE" << endl;
+        }
+    }
+    return 0;
+}
