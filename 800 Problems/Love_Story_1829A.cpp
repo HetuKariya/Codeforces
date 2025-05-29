233036d43bfa216ae2cf75213757d0a3445716b3
HetuKariya <131229641+HetuKariya@users.noreply.github.com>
2025-05-29 11:37:43 +0530

    Added 40th Question

---
+++ 800 Problems/Love_Story_1829A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t, a;
+    cin >> t;
+    string s1, s2 = "codeforces";
+    for (int i = 0; i < t; i++)
+    {
+        a = 0;
+        cin >> s1;
+        for (int i = 0; i < int(s1.length()); i++)
+        {
+            if (s1[i] != s2[i])
+                a++;
+        }
+        cout << a << "\n";
+    }
+    return 0;
+}
