5c2d618384a7360789d3892f3eabd21478e56206
HetuKariya <131229641+HetuKariya@users.noreply.github.com>
2025-06-18 23:04:26 +0530

    Added 52nd Question

---
+++ 800 Problems/Plus_or_Minus_1807A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t, a, b, c;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a >> b >> c;
+        if (a + b == c)
+            cout << "+" << "\n";
+        else
+            cout << "-" << "\n";
+    }
+    return 0;
+}
