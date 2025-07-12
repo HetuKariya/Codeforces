05053955309f0c16e6622d6af801297c9eb50157
HetuKariya <131229641+HetuKariya@users.noreply.github.com>
2025-07-12 20:08:29 +0530

    Added 65th Question

---
+++ 800 Problems/Vasya_the_Hipster_581A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int a, b;
+    cin >> a >> b;
+    if (a > b)
+        cout << b << " " << (a - b) / 2;
+    else
+        cout << a << " " << (b - a) / 2;
+    return 0;
+}
