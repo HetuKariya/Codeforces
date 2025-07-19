9d93a5b8fe0ce51fc891b8d4269cce5e06d452fb
HetuKariya <131229641+HetuKariya@users.noreply.github.com>
2025-07-19 11:34:53 +0530

    Added 208th Question

---
+++ 800 Problems/Minimize!_2009A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t, a, b;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a >> b;
+        cout << b - a << "\n";
+    }
+    return 0;
+}
