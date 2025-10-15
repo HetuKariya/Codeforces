bf27816cd4ed0a2f7a7e8c1d9699c37937bf5823
HetuKariya <131229641+HetuKariya@users.noreply.github.com>
2025-10-15 22:36:47 +0530

    Added 270th Question

---
+++ 1000 Problems/Cableway_90A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    cout.tie(0);
+    int a, b, c, maxans, ans1, ans2, ans3;
+    cin >> a >> b >> c;
+    ans1 = a / 2 + a % 2;
+    ans2 = b / 2 + b % 2;
+    ans3 = c / 2 + c % 2;
+    maxans = max(max(ans1, ans2), ans3);
+    if(maxans == ans3)
+        cout << 30 + 3 * (maxans - 1) + 2; 
+    else if(maxans == ans2)
+        cout << 30 + 3 * (maxans - 1) + 1;
+    else
+        cout << 30 + 3 * (maxans - 1);  
+    return 0; 
+} 
