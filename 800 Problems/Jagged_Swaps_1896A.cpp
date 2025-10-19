77ed1e724161f062cd2c3c8f15bc7f9847824f8b
HetuKariya <131229641+HetuKariya@users.noreply.github.com>
2025-10-19 22:35:38 +0530

    Added 275th Question

---
+++ 800 Problems/Jagged_Swaps_1896A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t, n;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        if (a[0] == 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
